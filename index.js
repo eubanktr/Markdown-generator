f2cc5b7def279a6a1af9fd291a3ee40262110851
Thomas Eubank <eubanktr@gmail.com>
2021-07-29 20:56:06 -0400

    pushing files

---
+++ index.js
@@ -0,0 +1,97 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    message: "Enter the project title:",
+    name: 'projTitle',
+  },
+  {
+    type: 'input',
+    message: "Please enter the description of your project:",
+    name: 'projDescription',
+  },
+  {
+    type: 'input',
+    message: "Enter installation notes:",
+    name: 'projNotes',
+  },
+  {
+    type: 'input',
+    message: "Please include usage information:",
+    name: 'projUsage',
+  },
+  {
+    type: 'input',
+    message: "Include guidelines for contribution:",
+    name: 'projContribution',
+  },
+  {
+    type: 'input',
+    message: "Include guidlines for testing:",
+    name: 'projTesting',
+  },
+  {
+    type: 'checkbox',
+    message: "Please select license(s) below:",
+    name: 'projLicense',
+    choices: [
+      'MIT',
+      'GNU AGPLv3',
+      'GNU GPLv3',
+      'GNU LGPLv3',
+      'Mozilla Public 2.0',
+      'Apache 2.0',
+      'Boost Software 1.0',
+      'The Unlicense'
+    ]
+  },
+  {
+    type: 'input',
+    message: "Enter your GitHub username:",
+    name: 'projGit',
+  },
+  {
+    type: 'input',
+    message: "Enter an email address for questions from users:",
+    name: 'projEmail',
+  },
+];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.appendFile(fileName, data), (err) =>
+//       err ? console.error(err) : console.log('Success')
+// };
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+  .prompt(questions)
+  
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+    const markDown = generateMarkdown(answers);
+   
+    fs.writeFile('testREADME.md', markDown, (err) =>
+      err ? console.log(err) : console.log('success')
+      );
+    })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong)
+    }
+  });
+}
+
+// Function call to initialize app
+init();
+
+// initializing write function
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,38 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+  return `# ${answers.projTitle}\n
+  TABLE OF CONTENTS
+  =========================
+  [Description](#Description)
+
+  ## Description
+  ${answers.projDescription}\n
+  ## Notes
+  ${answers.projNotes}\n
+  ## Usage Information
+  ${answers.projUsage}\n
+  ## Contributions
+  ${answers.projContribution}\n
+  ## Testing Information
+  ${answers.projTesting}\n
+  ## Licensing
+  ${answers.projLicense}\n
+  ## Questions
+  GitHub Username: https://github.com/${answers.projGit.toLowerCase().trim()}
+  If you have any questions please reach out via email. Email address: ${answers.projEmail}
+`;
+}
+
+module.exports = generateMarkdown;
